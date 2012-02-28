c47d8fc1438e2e6cb6030a59bb58690f8ce77006
Duncan Beevers <duncan@dweebd.com>
2012-02-28 09:06:51 -0600

    Indicate where partial was included

--- lib/rails_view_annotator/action_view/partial_renderer.rb
+++ lib/rails_view_annotator/action_view/partial_renderer.rb
@@ -1,11 +1,28 @@
 module RailsViewAnnotator
+  class ContextError < StandardError
+  end
+
   def self.augment_partial_renderer klass
     render = klass.instance_method :render
     klass.send(:define_method, :render) do |*args|
       inner = render.bind(self).call(*args)
       short_identifier = Pathname.new(identifier).relative_path_from Rails.root
-      if not inner.empty?
-        "<!-- begin: #{short_identifier} -->\n#{inner}<!-- end: #{short_identifier} -->".html_safe
+
+      backtrace = nil
+      begin
+        raise ContextError
+      rescue ContextError => e
+        backtrace = e.backtrace
+      end
+
+      r = /^#{Regexp.escape(Rails.root.to_s)}\/([^:]+:\d+)/
+      backtrace.find { |line| line.match r }
+      called_from = context = $1
+
+      descriptor = "#{short_identifier} (from #{called_from})"
+
+      if not inner.blank?
+        "<!-- begin: #{descriptor} -->\n#{inner}<!-- end: #{descriptor} -->".html_safe
       end
     end
     klass.send(:include, InstanceMethods)
